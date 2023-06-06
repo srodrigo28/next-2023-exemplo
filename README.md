3156dd29224ef0b084e8c88a81504928dc082be3
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-06-06 11:37:46 -0300

    updated-buugggg

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 ## Udemy Next com Leonardo Leitão
 * Link
+https://www.udemy.com/course/nextjs-e-react/learn/lecture/27421106#overview
 ```bash
     https://www.udemy.com/course/nextjs-e-react/learn/lecture/27421100#overview
 ```
--- src/pages/api/questoes/[id].ts
+++ src/pages/api/questoes/[id].ts
@@ -1,5 +1,13 @@
 import questoes from "../bancoDeQuestoes"
-
 export default (req, res) => {
-  res.status(200).json( questoes[0].paraObjeto())
+  const idSelecionado = +req.quey.id
+
+  const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+  
+  if (unicaQuestaoOuNada.length === 1) {
+    const questaoSelecionada = unicaQuestaoOuNada[0]
+    res.status(200).json(questaoSelecionada.paraObjeto())
+  } else {
+    res.status(204).send()
+  }
 }
