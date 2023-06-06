f07fc098ce463d9ac05ce62c4776bce24f8ab051
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-06-06 14:31:47 -0300

    api-banco-updated

--- src/pages/api/bancoDeQuestoes.ts
+++ src/pages/api/bancoDeQuestoes.ts
@@ -2,17 +2,29 @@ import QuestaoModal from "../../model/questao";
 import RespostaModel from "../../model/resposta";
 
 const questoes: QuestaoModal[] = [
-    new QuestaoModal(306, 'Qual bicho transmite a Doença de Chagas?', [
+    new QuestaoModal(10, 'Qual bicho transmite a Doença de Chagas?', [
         RespostaModel.errada('Abelha'),
         RespostaModel.errada('Barata'),
         RespostaModel.errada('Pulga'),
         RespostaModel.certa('Barbeiro'),
     ]),
-    new QuestaoModal(202, 'Qual fruto é conhecido no Norte e Nordeste como "Jerimum?', [
+    new QuestaoModal(11, 'Qual fruto é conhecido no Norte e Nordeste como "Jerimum?', [
         RespostaModel.errada('Caju'),
         RespostaModel.errada('Côco'),
         RespostaModel.errada('Chuchu'),
         RespostaModel.certa('Abóbora'),
+    ]),
+    new QuestaoModal(12, 'Qual o é coletivo de cães?', [
+        RespostaModel.errada('Manada'),
+        RespostaModel.errada('Alcateia'),
+        RespostaModel.errada('Rebanho'),
+        RespostaModel.certa('Matilha'),
+    ]),
+    new QuestaoModal(13, 'Qual é o triângulo que tem todos os lados diferentes', [
+        RespostaModel.errada('Equilátero'),
+        RespostaModel.errada('Isóceles'),
+        RespostaModel.errada('Trapézio'),
+        RespostaModel.certa('Escaleno'),
     ]),
 ]
 export default questoes
--- src/pages/api/questionario/index.ts
+++ src/pages/api/questionario/index.ts
@@ -1,3 +1,5 @@
+import questoes from "../bancoDeQuestoes"
+
 export default (req, res) => {
-    res.status(200).json({ teste: 123 })
+    res.status(200).json(questoes.map(questoes => questoes.id))
 }
